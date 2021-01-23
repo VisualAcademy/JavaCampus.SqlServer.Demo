3076957cc1b3e182d802605e43fd42f4c40d3090
JavaCampus <dev@javacampus.com>
2021-01-23 19:50:48 +0000

    Project Creation

---
+++ JavaCampus/src/main/java/com/hawaso/javacampus/models/Blog.java
@@ -0,0 +1,30 @@
+package com.hawaso.javacampus.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Blog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
